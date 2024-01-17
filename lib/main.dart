5a2b6d1f918188d6bc765a08f84261d2998c2546
ImashaNawodi <imashanaw1999@gmail.com>
2024-01-17 11:30:45 +0530

    Categories scrren is designed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:reusemart_app/consts/theme_data.dart';
+import 'package:reusemart_app/inner_screens/feeds_screen.dart';
+import 'package:reusemart_app/inner_screens/on_sale_screen.dart';
 import 'package:reusemart_app/provider/dark_theme_provider.dart';
 import 'package:reusemart_app/screens/btm_bar.dart';
 
@@ -16,40 +18,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-DarkThemeProvider themeChangeProvider =DarkThemeProvider();
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
 
   void getCurrentAppTheme() async {
-    themeChangeProvider.setDarkTheme = 
-    await themeChangeProvider.darkThemePrefs.getTheme();
-    
+    themeChangeProvider.setDarkTheme =
+        await themeChangeProvider.darkThemePrefs.getTheme();
   }
 
   @override
-  void initState () {
+  void initState() {
     getCurrentAppTheme();
     super.initState();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_)
-        {
+        ChangeNotifierProvider(create: (_) {
           return themeChangeProvider;
         })
       ],
-      child :Consumer<DarkThemeProvider> (
-        builder: (context,themeProvider, child) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: Styles.themeData(themeProvider.getDarkTheme
-, context),
-        home:const BottomBarScreen());
-  }),
+      child:
+          Consumer<DarkThemeProvider>(builder: (context, themeProvider, child) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: Styles.themeData(themeProvider.getDarkTheme, context),
+            home: const BottomBarScreen(),
+            routes: {
+              OnSaleScreen.routeName: (ctx) => const OnSaleScreen(),
+                 FeedsScreen.routeName: (ctx) => const FeedsScreen(),
+            });
+      }),
     );
   }
 }
-
--- lib/screens/btm_bar.dart
+++ lib/screens/btm_bar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:provider/provider.dart';
 import 'package:reusemart_app/provider/dark_theme_provider.dart';
-import 'package:reusemart_app/screens/cart.dart';
+import 'package:reusemart_app/screens/cart/cart_screen.dart';
 import 'package:reusemart_app/screens/categories.dart';
 import 'package:reusemart_app/screens/home_screen.dart';
 import 'package:reusemart_app/screens/user.dart';
@@ -15,7 +15,7 @@ class BottomBarScreen extends StatefulWidget {
 }
 
 class _BottomBarScreenState extends State<BottomBarScreen> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 2;
   final List<Map<String,dynamic>> _pages = [
     {'page' :const HomeScreen(),'title' : 'Home Screen'},
     {'page' :CategoriesScreen(),'title' : 'Categories Screen'},
